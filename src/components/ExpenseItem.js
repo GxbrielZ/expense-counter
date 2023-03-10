2290776a687a12a4e12513e9dee1064f24cdfa5b
Gabriel Zalewski <gabrielz2702@gmail.com>
2023-03-10 09:29:54 +0100

    Added some titles to the ExpenseItem file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+    return (
+        <div>
+            <div>February 27th 2023</div>
+            <div>
+                <h2>Birthday Gift</h2>
+                <div>$99.25</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
