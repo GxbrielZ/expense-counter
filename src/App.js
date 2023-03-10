9d439583fffaabb2ee55a1423df515b1f1e49de2
Gabriel Zalewski <gabrielz2702@gmail.com>
2023-03-10 19:41:02 +0100

    Created Card.js and Card.css file that are storing duplicate styling code

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ function App() {
   const expenses = [
     {
       id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
+      title: 'Potatoes',
+      amount: 5.99,
       date: new Date(2020, 7, 14),
     },
     {
       id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
+      title: 'Car',
+      amount: 2499.50,
       date: new Date(2021, 2, 12) },
     {
       id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
+      title: 'Pizza',
+      amount: 9.76,
       date: new Date(2021, 2, 28),
     },
     {
       id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
+      title: 'Fridge',
+      amount: 260,
       date: new Date(2021, 5, 12),
     },
   ];
