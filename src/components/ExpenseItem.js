e283e54c325ebb9d4b0590f4eb34765999145b5c
Gabriel Zalewski <gabrielz2702@gmail.com>
2023-03-10 09:55:16 +0100

    Created dynamic data in ExpenseItem file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2023, 2, 27);
+    const expenseTitle = 'Birthday Gift';
+    const ExpenseAmount = 99.25;
+
     return (
         <div className='expense-item'>
-            <div>February 27th 2023</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Birthday Gift</h2>
-                <div className='expense-item__price'>$99.25</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${ExpenseAmount}</div>
             </div>
         </div>
     );
